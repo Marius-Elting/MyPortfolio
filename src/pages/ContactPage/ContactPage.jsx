d596e6ac98869c1592d974fcc19cdb49176892ce
Marius <Marius.Supercode@gmail.com>
2023-03-04 16:44:40 +0100

    edited Texts and CV

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,32 +1,32 @@
-import "./ContactPage.css"
-import { useState, useRef } from "react"
-import { Link } from "react-router-dom"
-import MyPicture from "../../components/Picture/Picture"
-import { Helmet } from "react-helmet"
-import Data from "../../Languages.json"
+import "./ContactPage.css";
+import { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import MyPicture from "../../components/Picture/Picture";
+import { Helmet } from "react-helmet";
+import Data from "../../Languages.json";
 
 function ContactPage() {
-    const [emailStatus, setEmailStatus] = useState()
-    const [nameStatus, setNameStatus] = useState()
-    const emailRef = useRef()
-    const subjectRef = useRef()
-    const nameRef = useRef()
-    const textRef = useRef()
-    const acceptPolicy = useRef()
+    const [emailStatus, setEmailStatus] = useState();
+    const [nameStatus, setNameStatus] = useState();
+    const emailRef = useRef();
+    const subjectRef = useRef();
+    const nameRef = useRef();
+    const textRef = useRef();
+    const acceptPolicy = useRef();
 
-    const lang = sessionStorage.getItem("Language")
-    const { ContactMe, Other } = Data[lang]
+    const lang = sessionStorage.getItem("Language");
+    const { ContactMe, Other } = Data[lang];
 
     const sendMail = (e) => {
-        e.preventDefault()
-        if (!emailRef.current.value) setEmailStatus("Error")
-        if (!subjectRef.current.value) subjectRef.current.style.border = "3px solid red"
-        if (!nameRef.current.value) nameRef.current.style.border = "3px solid red"
-        if (!textRef.current.value) textRef.current.style.border = "3px solid red"
-        if (!acceptPolicy.current.checked) acceptPolicy.current.style.outline = "3px solid red"
+        e.preventDefault();
+        if (!emailRef.current.value) setEmailStatus("Error");
+        if (!subjectRef.current.value) subjectRef.current.style.border = "3px solid red";
+        if (!nameRef.current.value) nameRef.current.style.border = "3px solid red";
+        if (!textRef.current.value) textRef.current.style.border = "3px solid red";
+        if (!acceptPolicy.current.checked) acceptPolicy.current.style.outline = "3px solid red";
 
-        if (!emailRef.current.value || !subjectRef.current.value || !nameRef.current.value || !textRef.current.value || !acceptPolicy.current.checked) return
-        const form = new FormData(e.target)
+        if (!emailRef.current.value || !subjectRef.current.value || !nameRef.current.value || !textRef.current.value || !acceptPolicy.current.checked) return;
+        const form = new FormData(e.target);
         fetch(`${process.env.REACT_APP_URL}/contactme`,
             {
                 method: "POST",
@@ -34,31 +34,31 @@ function ContactPage() {
             }).then(res => res.json())
             .then(res => {
                 if (res === "Email Error") {
-                    setEmailStatus("Error")
+                    setEmailStatus("Error");
                 } else {
-                    setEmailStatus("OK")
-                    e.target.reset()
+                    setEmailStatus("OK");
+                    e.target.reset();
                 }
-            })
-    }
+            });
+    };
 
     function isValidEmailAddress(address) {
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (re.test(address)) {
-            setEmailStatus("OK")
+            setEmailStatus("OK");
         } else {
-            setEmailStatus("Error")
+            setEmailStatus("Error");
         }
     }
 
     function containsNumbers(e, str) {
-        let re = /[0-9]/
+        let re = /[0-9]/;
         if (!re.test(str) && e.target.value.length > 0) {
-            e.target.style.border = "3px solid green"
-            setNameStatus("OK")
+            e.target.style.border = "3px solid green";
+            setNameStatus("OK");
         } else {
-            e.target.style.border = "3px solid red"
-            setNameStatus("Error")
+            e.target.style.border = "3px solid red";
+            setNameStatus("Error");
         }
     }
     return (
@@ -78,7 +78,7 @@ function ContactPage() {
                     <input
                         id="yourName"
                         onChange={(e) => {
-                            containsNumbers(e, e.target.value)
+                            containsNumbers(e, e.target.value);
                         }}
                         ref={nameRef} type="text" name="senderName" placeholder={ContactMe.YName}
                     >
@@ -116,9 +116,9 @@ function ContactPage() {
                         id="yourSubject"
                         onChange={(e) => {
                             if (e.target.value.length > 0) {
-                                e.target.style.border = "3px solid green"
+                                e.target.style.border = "3px solid green";
                             } else {
-                                e.target.style.border = "3px solid red"
+                                e.target.style.border = "3px solid red";
                             }
                         }}
                         ref={subjectRef} type="text" name="subject" placeholder={ContactMe.YSubject}
@@ -129,9 +129,9 @@ function ContactPage() {
                         id="yourMessage"
                         onChange={(e) => {
                             if (e.target.value.length > 0) {
-                                e.target.style.border = "3px solid green"
+                                e.target.style.border = "3px solid green";
                             } else {
-                                e.target.style.border = "3px solid red"
+                                e.target.style.border = "3px solid red";
                             }
                         }}
                         ref={textRef} name="message" placeholder={ContactMe.YMessage}
@@ -139,7 +139,7 @@ function ContactPage() {
                     </textarea>
                     <div className="ContactPage-acceptData">
                         <label htmlFor="acceptDatenschutz"><input ref={acceptPolicy} type="checkbox" id="acceptDatenschutz" />{ContactMe.Acceptpolicy}</label>
-                        <p>{ContactMe.Infopolicy} {<Link to="/privatepolicy">{ContactMe.policyLink}</Link>}</p>
+                        <p>{ContactMe.Infopolicy} {<Link to="/privatepolicy">{ContactMe.policyLink}</Link>} {lang === "German" ? "gelesen haben." : ""}</p>
                     </div>
                     {/* <CustomButton type={"submit"} size={25} linkTo="/contactme">Send</CustomButton> */}
                     <button type="submit">Send</button>
